861dfd287d5f221cbe156eb7adb991a37bec5823
mediodiablo <luis@arrieta.io>
2025-08-10 22:32:51 -0700

    changed font from JetBrains Mono to Jost

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,52 +2,51 @@ import type { SiteConfig } from '@types'
 
 const config: SiteConfig = {
   // Absolute URL to the root of your published site, used for generating links and sitemaps.
-  site: 'https://multiterm.stelclementine.com',
+  site: 'https://cojudeces.com',
   // The name of your site, used in the title and for SEO.
-  title: 'MultiTerm',
+  title: 'cojudeces.com',
   // The description of your site, used for SEO and RSS feed.
   description:
-    'A coder-ready Astro blog theme with 59 of your favorite color schemes to choose from',
+    'compartiendo cosas inútiles y sin importancia desde el 2006',
   // The author of the site, used in the footer, SEO, and RSS feed.
-  author: 'Katy Kookaburra',
+  author: 'el escuadrón',
   // Keywords for SEO, used in the meta tags.
-  tags: ['Astro', 'Terminal', 'Theme', 'MultiTerm', 'stelcodes'],
+  tags: ['astro', 'mediodiablo', 'cojudeces'],
   // Path to the image used for generating social media previews.
   // Needs to be a square JPEG file due to limitations of the social card generator.
   // Try https://squoosh.app/ to easily convert images to JPEG.
   socialCardAvatarImage: './src/content/avatar.jpg',
   // Font imported from @fontsource or elsewhere, used for the entire site.
   // To change this see src/styles/global.css and import a different font.
-  font: 'JetBrains Mono Variable',
+  font: 'Jost Variable',
   // For pagination, the number of posts to display per page.
   pageSize: 5,
   // The navigation links to display in the header.
   navLinks: [
     {
-      name: 'Home',
+      name: 'home',
       url: '/',
     },
     {
-      name: 'About',
+      name: 'about',
       url: '/about',
     },
     {
-      name: 'Archive',
+      name: 'arch.chivo',
       url: '/posts',
     },
     {
-      name: 'GitHub',
-      url: 'https://github.com/stelcodes/multiterm-astro',
-      external: true,
+      name: 'links',
+      url: '/links',
     },
   ],
   // The theming configuration for the site.
   themes: {
     // The theming mode. One of "single" | "select" | "light-dark-auto".
-    mode: 'select',
+    mode: 'single',
     // The default theme identifier, used when themeMode is "select" or "light-dark-auto".
     // Make sure this is one of the themes listed in `themes` or "auto" for "light-dark-auto" mode.
-    default: 'catppuccin-mocha',
+    default: 'synthwave-84',
     // Shiki themes to bundle with the site.
     // https://expressive-code.com/guides/themes/#using-bundled-themes
     // These will be used to theme the entire site along with syntax highlighting.
