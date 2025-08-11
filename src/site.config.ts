34df9fbb9179bcd0b45488393bf8457992acf8eb
mediodiablo <luis@arrieta.io>
2025-08-11 16:43:18 -0700

    removed github calendar, removed addendum to blog posts

--- src/site.config.ts
+++ src/site.config.ts
@@ -46,7 +46,7 @@ const config: SiteConfig = {
     mode: 'single',
     // The default theme identifier, used when themeMode is "select" or "light-dark-auto".
     // Make sure this is one of the themes listed in `themes` or "auto" for "light-dark-auto" mode.
-    default: 'synthwave-84',
+    default: 'slack-ochin',
     // Shiki themes to bundle with the site.
     // https://expressive-code.com/guides/themes/#using-bundled-themes
     // These will be used to theme the entire site along with syntax highlighting.
